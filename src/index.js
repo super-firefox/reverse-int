0e52bc4acaf15dbd7ddc29872b1392e0d725f0b5
Ihor RSS <superfirfox@gmail.com>
2021-07-14 17:19:32 +0300

    feat: finish reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  if(n < 0){
+      n *= -1;
+  }
+
+  while(n % 10 === 0){
+    n /= 10;
+  }
+
+  return +(n.toString().split("").reverse().join(""))
 }
